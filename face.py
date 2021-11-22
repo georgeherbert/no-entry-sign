ff6e397b02feb415d39520c3c5303ebda1d88198
georgeherbert <george.herbert@icloud.com>
2021-11-22 23:16:10 +0000

    Commented out implementation of stop sign detection in face.py

--- face.py
+++ face.py
@@ -23,9 +23,9 @@ def detect_and_display(frame, cascade):
 def main():
     frame = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
     
-    cascade = cv2.CascadeClassifier("files_provided/frontalface.xml")
-    # cascade.load()
-    
+    cascade = cv2.CascadeClassifier("files_provided/Noentrycascade/frontalface.xml")
+    # cascade = cv2.CascadeClassifier("files_provided/Noentrycascade/cascade.xml")
+
     detect_and_display(frame, cascade)
     
 if __name__ == "__main__":
