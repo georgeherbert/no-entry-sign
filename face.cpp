7609eb5bd0befd47e6d44ad3185ef327032bc10e
georgeherbert <george.herbert@icloud.com>
2021-11-23 19:13:03 +0000

    Removed

--- face.cpp
+++ face.cpp
@@ -63,7 +63,7 @@ void detectAndDisplay( Mat frame )
        // 4. Draw box around faces found
 	for( int i = 0; i < faces.size(); i++ )
 	{
-		cout << faces[i].x << " " << faces[i].y;
+		// cout << faces[i].x << " " << faces[i].y;
 		// string face_pos = std::to_string(faces[i].x) + " " + std::to_string(faces[i].y) + " " + std::to_string(faces[i].width) + " " + std::to_string(faces[i].height);
 		// std::cout << face_pos;
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
