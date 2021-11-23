59309312597b98c48ef6195b6b823e6bbfa38f54
georgeherbert <george.herbert@icloud.com>
2021-11-23 21:47:59 +0000

    Now displays rectangles that face_cpp displayed

--- iou.py
+++ iou.py
@@ -1,11 +1,21 @@
 import numpy as np
 import cv2
 
-def main():
-    with open("faces_detected/coordinates/0.txt") as f:
+NUM = 0
+
+def get_face_cpp_rectangles():
+    with open(f"faces_detected/coordinates/{NUM}.txt") as f:
         lines = f.readlines()
-        lines_cleaned = [line.strip() for line in lines]
-        print(lines_cleaned)
+    return [list(map(int, line.strip().split(" "))) for line in lines]
+
+def main():
+    image = cv2.imread(f"No_entry/NoEntry{NUM}.bmp", cv2.IMREAD_COLOR)
+    rectangles = get_face_cpp_rectangles()
+    for (x, y, w, h) in rectangles:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow("Display window", image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
