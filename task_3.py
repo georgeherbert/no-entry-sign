93a1dec71b85414194177a715f221ce75627ad0b
georgeherbert <george.herbert@icloud.com>
2021-11-23 10:20:46 +0000

    Modified circle code and copied over

--- task_3.py
+++ task_3.py
@@ -12,10 +12,11 @@ KERNEL_DERIVATIVE_Y = np.array([
     [0, 0, 0],
     [-1, -2, -1]
 ])
-MINIMUM_RADIUS = 10
+MINIMUM_RADIUS = 20
 MAXIMUM_RADIUS = 90
 
 T_S = 250
+T_H = 100
 
 def convolution(image, kernel):
     kernel_flipped = np.flip(kernel)
@@ -46,10 +47,10 @@ def sobel(image):
     gradient_direction = np.arctan2(dy, dx)
     gradient_direction_display = normalise(gradient_direction)
 
-    cv2.imwrite("task3/dx_display.jpg", dx_display)
-    cv2.imwrite("task3/dy_display.jpg", dy_display)
-    cv2.imwrite("task3/gradient_magnitude.jpg", gradient_magnitude_display)
-    cv2.imwrite("task3/gradient_direction.jpg", gradient_direction_display)
+    cv2.imwrite("task_3/dx_display.jpg", dx_display)
+    cv2.imwrite("task_3/dy_display.jpg", dy_display)
+    cv2.imwrite("task_3/gradient_magnitude.jpg", gradient_magnitude_display)
+    cv2.imwrite("task_3/gradient_direction.jpg", gradient_direction_display)
 
     return gradient_magnitude, gradient_direction
 
@@ -64,6 +65,7 @@ def hough(gradient_magnitude, gradient_direction):
     gradient_magnitude_threshold = gradient_magnitude.copy()
     gradient_magnitude_threshold[gradient_magnitude_threshold < T_S] = 0
     gradient_magnitude_threshold[gradient_magnitude_threshold >= T_S] = 255
+    cv2.imwrite("task_3/gradient_magnitude_threshold.jpg", gradient_magnitude_threshold)
 
     for x in range(image_height):
         for y in range(image_width):
@@ -73,13 +75,10 @@ def hough(gradient_magnitude, gradient_direction):
                     y_0 = int(y - (r + MINIMUM_RADIUS) * np.cos(gradient_direction[x][y]))
                     if x_0 > 0 and x_0 < image_height and y_0 > 0 and y_0 < image_width:
                         hough_space[x_0][y_0][r] += 1
-                    # Add pi to the gradient angle so the angle also points in the other direction
-                    # We don't know if the gradient is pointing towards or away from the center of the circle.
-                    # https://stackoverflow.com/questions/48279460/unable-to-properly-calculate-a-b-space-in-hough-transformation-for-circle-det
-                    x_0 = int(x - (r + MINIMUM_RADIUS) * np.sin(gradient_direction[x][y] + np.pi))
-                    y_0 = int(y - (r + MINIMUM_RADIUS) * np.cos(gradient_direction[x][y] + np.pi))
-                    if x_0 > 0 and x_0 < image_height and y_0 > 0 and y_0 < image_width:
-                        hough_space[x_0][y_0][r] += 1
+                    # x_0 = int(x - (r + MINIMUM_RADIUS) * np.sin(gradient_direction[x][y] + np.pi))
+                    # y_0 = int(y - (r + MINIMUM_RADIUS) * np.cos(gradient_direction[x][y] + np.pi))
+                    # if x_0 > 0 and x_0 < image_height and y_0 > 0 and y_0 < image_width:
+                    #     hough_space[x_0][y_0][r] += 1
 
     return hough_space
 
@@ -89,14 +88,19 @@ def display_hough_space(hough_space):
     return summed_hough_space_display
 
 def main():
-    image = cv2.imread(".png")
-    image_grey = cv2.cvtColor(src = image, code = cv.COLOR_BGR2GRAY)
+    image = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
+    image_grey = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
 
     gradient_magnitude, gradient_direction = sobel(image_grey)
 
     hough_space = hough(gradient_magnitude, gradient_direction,)
     hough_space_display = display_hough_space(hough_space)
     cv2.imwrite("task_3/summed_hough_space.jpg", hough_space_display)
+
+    hough_space_threshold = hough_space_display.copy()
+    hough_space_threshold[hough_space_threshold < T_H] = 0
+    hough_space_threshold[hough_space_threshold >= T_H] = 255
+    cv2.imwrite("task_3/summed_hough_space_threshold.jpg", hough_space_threshold)
 
     image_height = gradient_magnitude.shape[0]
     image_width = gradient_magnitude.shape[1]
@@ -108,7 +112,7 @@ def main():
             for r in range(MAXIMUM_RADIUS - MINIMUM_RADIUS):
                 if hough_space[x][y][r] >= t_h:
                     print(hough_space[x][y][r])
-                    cv.circle(image, (y, x), r + MINIMUM_RADIUS, (0, 0, 255), 2)
+                    cv2.circle(image, (y, x), r + MINIMUM_RADIUS, (0, 0, 255), 2)
 
     cv2.imshow("Display window", image)
     cv2.waitKey(0)
