9b8322a2cffcea23bc16f619e60edc951ce772ba
georgeherbert <george.herbert@icloud.com>
2021-11-23 21:49:58 +0000

    Renamed the coordinates folder to face_cpp_bounding_boxes to be more descript

--- iou.py
+++ iou.py
@@ -3,15 +3,15 @@ import cv2
 
 NUM = 0
 
-def get_face_cpp_rectangles():
-    with open(f"faces_detected/coordinates/{NUM}.txt") as f:
+def get_face_cpp_bounding_boxes():
+    with open(f"faces_detected/face_cpp_bounding_boxes/{NUM}.txt") as f:
         lines = f.readlines()
     return [list(map(int, line.strip().split(" "))) for line in lines]
 
 def main():
     image = cv2.imread(f"No_entry/NoEntry{NUM}.bmp", cv2.IMREAD_COLOR)
-    rectangles = get_face_cpp_rectangles()
-    for (x, y, w, h) in rectangles:
+    boxes = get_face_cpp_bounding_boxes()
+    for (x, y, w, h) in boxes:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.imshow("Display window", image)
     cv2.waitKey()
